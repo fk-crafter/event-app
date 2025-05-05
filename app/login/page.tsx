e11ff0a1ef16573d2e7e3d3decee5e87d60e669b
fk-crafter <fowad.kh@outlook.fr>
2025-05-05 11:55:47 +0200

    added mockup section

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+import { LoginModal } from "@/components/LoginModal";
+import { BackgroundBeams } from "@/components/ui/background-beams";
+export default function LoginPage() {
+  return (
+    <div className="relative min-h-screen flex items-center justify-center bg-background">
+      <LoginModal />
+      <BackgroundBeams />
+    </div>
+  );
+}
