281fc497a3ddff1961062b1f996c9ff984433acc
fk-crafter <fowad.kh@outlook.fr>
2025-06-02 14:47:32 +0200

    feat: allow guest users to access vote page without login and split layout

---
+++ frontend/app/app/(protected)/layout.tsx
@@ -0,0 +1,18 @@
+import Sidebar from "@/components/Sidebar";
+import AppHeader from "@/components/HeaderApp";
+
+export default function ProtectedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <AppHeader />
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="flex-1 p-6">{children}</main>
+      </div>
+    </div>
+  );
+}
--- frontend/app/app/layout.tsx
+++ frontend/app/app/layout.tsx
@@ -1,15 +1,3 @@
-import Sidebar from "@/components/Sidebar";
-import AppHeader from "@/components/HeaderApp";
-
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <AppHeader />
-
-      <div className="flex flex-1">
-        <Sidebar />
-        <main className="flex-1 p-6">{children}</main>
-      </div>
-    </div>
-  );
+  return <main className="min-h-screen p-6">{children}</main>;
 }
