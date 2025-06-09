3f03804653f50ece69c51c7b61d824356d30661b
fk-crafter <fowad.kh@outlook.fr>
2025-06-09 11:08:49 +0200

    fix(AuthCallback): wrapped in a suspense boundary

--- frontend/app/auth/callback/page.tsx
+++ frontend/app/auth/callback/page.tsx
@@ -1,23 +1,14 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import AuthCallbackContent from "./AuthCallbackContent";
 
 export default function AuthCallbackPage() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const token = searchParams.get("token");
-
-    if (token) {
-      localStorage.setItem("token", token);
-
-      router.push("/app/create-event");
-    } else {
-      router.push("/login");
-    }
-  }, [router, searchParams]);
-
-  return <p className="text-center mt-20">Processing login...</p>;
+  return (
+    <Suspense
+      fallback={<p className="text-center mt-20">Processing login...</p>}
+    >
+      <AuthCallbackContent />
+    </Suspense>
+  );
 }
